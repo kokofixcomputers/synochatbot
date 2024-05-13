3c1187961b2e0c5772e071f08467cc80e44345cd
kokofixcomputers <github@kokofixcomputers.serv00.net>
2024-05-13 08:10:01 -0700

    rename

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='synochat',
+    name='synochatbot',
     version='1.0',
     author='kokofixcomputers',
     packages=find_packages(),
